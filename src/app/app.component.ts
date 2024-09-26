485948a7c4af5cd8c95d5777bdea9ed8cd2f55ad
Lord17Charly <gutierrezvottecarlos@gmail.com>
2024-09-25 22:43:13 -0600

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { EducationComponent } from './education/education.component';
+import { InterestsComponent } from "./interests/interests.component";
+import { LenguagesComponent } from "./lenguages/lenguages.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { AchievementsComponent } from "./achievements/achievements.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,WorkExperienceComponent,EducationComponent],
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, EducationComponent, InterestsComponent, LenguagesComponent, SkillsComponent, AchievementsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
